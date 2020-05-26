e0eb1a4023427839bd18df6ccee5d227a4889589
Scott Kolby <skolby42@gmail.com>
2020-05-26 15:58:21 -0700

    Updated get rotation

--- Source/BattleTank/Private/TankBarrel.cpp
+++ Source/BattleTank/Private/TankBarrel.cpp
@@ -9,7 +9,7 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 	// Rotate barrel up/down given a max elevation speed and frame time
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
 	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto RawNewElevation = GetRelativeRotation().Pitch + ElevationChange;
 	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
 	
 	SetRelativeRotation(FRotator(Elevation, 0, 0));
--- Source/BattleTank/Private/TankTurret.cpp
+++ Source/BattleTank/Private/TankTurret.cpp
@@ -9,7 +9,7 @@ void UTankTurret::Rotate(float RelativeSpeed)
 	// Rotate turret left/right given a max rotation speed and frame time
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
 	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	auto Rotation = RelativeRotation.Yaw + RotationChange;
+	auto Rotation = GetRelativeRotation().Yaw + RotationChange;
 
 	SetRelativeRotation(FRotator(0, Rotation, 0));
 }
