0d6b5480fd875e32af374d829c96a05a9ac4cd81
Scott Kolby <skolby42@gmail.com>
2020-05-28 12:06:04 -0700

    Nullptr check

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -10,6 +10,9 @@ void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (!GetPawn())
+		return;
+
 	auto AimingComponent = GetPawn()->FindComponentByClass<UTankAimingComponent>();
 	if (ensure(AimingComponent))
 		FoundAimingComponent(AimingComponent);
